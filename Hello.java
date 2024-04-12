e73ae69a9ce8de5f3428c1ef115e0c816824a474
Rojan <777sakusaa@gmail.com>
2024-04-12 12:27:23 +0545

    First question

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello{
 
  public static void main(String[] args) {
+   /*1. Write a program that prints that Hello World! to the console.Save the program as Hello.java */
     System.out.println("Hello World!");
 
  }   
